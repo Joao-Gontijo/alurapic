05b1e5cd98079d3b5f71f7b556f0a0d8252486be
Joao-Gontijo <jvgontijo166@gmail.com>
2021-12-14 14:43:56 -0300

    estrutura css paineis

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="corpo">
+    
     <h1 class="centralizado">{{ titulo }}</h1>
 
     <ul class="lista-fotos">
       <li class="lista-fotos-itens" v-for="foto in fotos">
-        <img :src="foto.url" :alt="foto.titulo">
+        <div class="painel">
+          <h1 class="painel-titulo">{{ foto.titulo }}</h1>
+          <div class="painel-conteudo">
+            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+          </div>
+        </div>
+        
       </li>
     </ul>
   </div>
@@ -43,5 +50,27 @@ export default {
 }
 .lista-fotos-itens{
   display: inline-block;
+}
+.imagem-responsiva{
+  width: 100%;
+}
+.painel{
+  padding: 0 auto;
+  border: solid 2px grey;
+  display: inline-block;
+  margin: 5px;
+  box-shadow: 5px 5px 10px grey;
+  width: 200px;
+  height: 100%;
+  vertical-align: top;
+  text-align: center;
+}
+.painel .painel-titulo{
+  text-align: center;
+  border: solid 2px;
+  background: lightblue;
+  margin: 0 0 15px 0;
+  padding: 10px;
+  text-transform: uppercase;
 }
 </style>
