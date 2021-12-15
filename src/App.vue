819acc1f3ad8ced196688cb7b80847f087eabd5b
Joao-Gontijo <jvgontijo166@gmail.com>
2021-12-15 16:42:40 -0300

    criando rotas

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,12 @@
 <template>
   <div class="corpo">    
-    <h1 class="centralizado">{{ titulo }}</h1>
-    <!-- v-bind da fonte de dados para view // v-on da view para fonte de dados -->
-    <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="Filtrar pelo titulo">
-
-    <ul class="lista-fotos">
-      <li class="lista-fotos-itens" v-for="foto in fotosComFiltro">
-        
-        <meu-painel :titulo="foto.titulo">
-            <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
-        </meu-painel> 
-
-        
-      </li>
-    </ul>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import Painel from './components/shared/painel/Painel.vue';
-import ImagemResponsiva from './components/shared/imagem-responsiva/ImagemResponsiva.vue';
+
 export default {
-  components:{
-    'meu-painel': Painel,
-    'imagem-responsiva': ImagemResponsiva
-  },
-  data(){
-    return{
-      titulo: 'Alurapic',
-      fotos: [],
-      filtro: ''
-    }
-  },
-  computed: {
-    fotosComFiltro(){
-      if(this.filtro){
-          //expressão regular que baseado no valor digitado buscado na lista de fotos
-          //trazendo do array que contém o que foi digitado (!case sensitive)
-          let exp = new RegExp(this.filtro.trim(), 'i');
-          return this.fotos.filter(foto => exp.test(foto.titulo));
-      } else {
-        return this.fotos;
-      }
-    }
-  },
-  created(){
-    let promise = this.$http.get('http://localhost:3000/v1/fotos');
-    //usar arrow function automaticamente retorna o valor de res.json, que tbm é uma promise
-    promise
-      .then(res => res.json()) //devolve a resposta do servidor e transforma em json
-      .then(fotos => this.fotos = fotos,  //this.fotos recebe as fotos que vem da api
-      err => console.log(err)); //caso dê erro no servidor, imprime no console 
-  }
 }
 </script>
 
@@ -60,19 +15,5 @@ export default {
     font-family: Arial, Helvetica, sans-serif;
     width: 96%;
     margin: 0 auto;
-  }
-  .centralizado{
-    text-align: center;
-  }
-  .lista-fotos{
-    list-style: none;
-  }
-  .lista-fotos-itens{
-    display: inline-block;
-  }
-
-  .filtro{
-    display: inline-block;
-    width: 100%;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
 import Vue from 'vue'  //global view object 
 import App from './App.vue' //componente
-
 import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
+import { routes } from './routes';
+
 Vue.use(VueResource);
+Vue.use(VueRouter);
+
+const router = new VueRouter({ 
+  routes,
+  mode: 'history'
+});
 
 new Vue({  //instancia de uma view = é a cola do componente e da view
   el: '#app', //el: elemento lá do index html
+  router,
   render: h => h(App) //função render
 })
 
