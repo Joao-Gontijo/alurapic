db0e004377f4cfd432783dc0d0a7750a8dbf9cb7
Joao-Gontijo <jvgontijo166@gmail.com>
2021-12-16 14:29:25 -0300

    melhorando componente botao

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -9,7 +9,12 @@
         
         <meu-painel :titulo="foto.titulo">
             <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
-            <meu-botao @botaoAtivado="remove(foto)" tipo="button" rotulo="remover"/>
+            <meu-botao 
+              tipo="button" rotulo="remover"
+              @botaoAtivado="remove(foto)"
+              :confirmacao="true"
+              estilo="perigo"
+            /> 
         </meu-painel> 
 
         
