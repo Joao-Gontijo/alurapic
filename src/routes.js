ec552e299fe9c54cd3b85c7bfc753072535a4fd0
Joao-Gontijo <jvgontijo166@gmail.com>
2021-12-22 17:37:42 -0300

    separando componente cadastro na build

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,8 @@
-import Cadastro from './components/cadastro/Cadastro.vue';
+//import Cadastro from './components/cadastro/Cadastro.vue';
+const Cadastro = () => 
+    System.import('./components/cadastro/Cadastro.vue')
+        .then(m => m.default);
+
 import Home from './components/home/Home.vue';
 
 export const routes = [
